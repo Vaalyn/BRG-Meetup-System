8fe7d27f65de71652c1228ff635e4fb607f14d9e
Vaalyn <vaalyndev@gmail.com>
2020-08-29 14:25:51 +0200

    Update date of third day

--- template/schedule/third-day.php
+++ template/schedule/third-day.php
@@ -1,6 +1,6 @@
 <div class="card color-3">
 	<div class="card-content grey-text text-lighten-2">
-		<h3 class="card-title center">Dritter Tag (10.05.2020)</h3>
+		<h3 class="card-title center">Dritter Tag (09.05.2021)</h3>
 		<div class="divider"></div>
 
 		<div class="row">
